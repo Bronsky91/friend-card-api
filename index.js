643c9da32862398b817c20a20c995fc2b8588a22
Bryan Reed <bryan87reed@gmail.com>
2022-06-28 11:20:50 -0700

    Set CORS policy

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(bodyParser.json());
 app.use("/api", routes);
 
